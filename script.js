8bbe8cdf54de7542a7825e180a14606c70ccda9f
demonicsaibot <77d7a77@gmail.com>
2022-02-15 13:52:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 import Header from "./js/header.js"
 import LoginForm from "./js/modal/loginForm.js"
-// import CardBody from "./js/card_body.js"
 import Footer from "./js/footer.js"
 import Filter from './js/filter.js';
 
 export const header = new Header();
 new Filter();
 new LoginForm();
-    // new CardBody()
 new Footer();
